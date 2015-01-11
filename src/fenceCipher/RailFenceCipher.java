ee832daeb0cced97ed1bbd1e8f3cf739371a1f01
Michael Happel <happel.michael@googlemail.com>
2015-01-11 14:16:41 +0100

    initial commit

---
+++ src/fenceCipher/RailFenceCipher.java
@@ -0,0 +1,11 @@
+package fenceCipher;
+
+/**
+ * Created by michaelhappel on 11/01/15.
+ */
+public class RailFenceCipher {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
