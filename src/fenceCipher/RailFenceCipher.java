614ff7d4fcbbfea8979906285fe0994fc79cca3a
nixrod <happel.michael@gmail.com>
2015-01-13 15:49:44 +0100

    Started with decryption

--- src/fenceCipher/RailFenceCipher.java
+++ src/fenceCipher/RailFenceCipher.java
@@ -12,32 +12,33 @@ import java.util.Map;
 public class RailFenceCipher {
 
     public static void main(String[] args) {
-        System.out.println(encrypt("LOLOLOLOLOLOLOLOLO", 2));
+        //System.out.println(encrypt("LOLOLOLOLOLOLOLOLO", 2));
+        decrypt("RIMIRAREDTORALPORMEDCDYGM", 3);
     }
 
     /**
-     * Encrypts a cleartext string by applying the railFence encryption of the given fenceLevel
+     * Encrypts a cleartext string by applying the railFence encryption of the given fenceHeight
      * @param cleartext The string to encrypt.
-     * @param fenceLevel The height of the fence which is used for the encryption.
+     * @param fenceHeight The height of the fence which is used for the encryption.
      * @return String containing the cipher
      */
-    protected static String encrypt(String cleartext, int fenceLevel) {
+    protected static String encrypt(String cleartext, int fenceHeight) {
         StringBuilder cypherBuilder = new StringBuilder();
-        Map<Integer, List<Integer>> fenceDistances = calculateFenceDistances(fenceLevel);
+        Map<Integer, List<Integer>> fenceDistances = calculateFenceDistances(fenceHeight);
 
         // build all fence level strings
-        for (int i = 0; i < fenceLevel; i++) {
+        for (int i = 0; i < fenceHeight; i++) {
             // indicates the next char for the current fence Level
             int nextCharToProcess = i;
             // indicates if we are in a fence down or upstroke
             boolean isDownstroke = true;
 
-            // grab all chars for current fenceLevel
+            // grab all chars for current fenceHeight
             while(nextCharToProcess < cleartext.length()) {
                 cypherBuilder.append(cleartext.charAt(nextCharToProcess));
 
                 int nextDistanceKey = (isDownstroke) ? 0 : 1;
-                nextCharToProcess += fenceDistances.get(new Integer(i)).get(new Integer(nextDistanceKey));
+                nextCharToProcess += fenceDistances.get(i).get(nextDistanceKey);
 
                 // changes stroke direction
                 isDownstroke = !isDownstroke;
@@ -47,22 +48,61 @@ public class RailFenceCipher {
         return cypherBuilder.toString();
     }
 
+    protected static String decrypt (String cypher, int fenceHeight) {
+        StringBuilder plaintextBuilder = new StringBuilder();
+
+        Map<Integer, List<Integer>> fenceDistances = calculateFenceDistances(fenceHeight);
+        // The length of a full fence segment, on which the pattern of the fence starts repeating itself again.
+        int segmentLength = fenceDistances.get(0).get(0);
+        int fullSegments = cypher.length() / segmentLength;
+
+        // Determine the rail boundaries in the cypher.
+        Map<Integer, Integer> railLengths = new HashMap<Integer, Integer>();
+        for (int i = 0; i < fenceHeight; i++) {
+            int railLength = 0;
+            // a fence tip occurs once in a full segment, all other parts twice.
+            int occurrenceMultiplier = (i == 0 || i == fenceHeight - 1) ? 1 : 2;
+            railLength += occurrenceMultiplier * fullSegments;
+
+            // count possible occurrences in last (fragmented) segment.
+            int fragmentLength = cypher.length() % segmentLength;
+            if (fragmentLength - i > fenceDistances.get(i).get(0)) {
+                railLength += 2;
+            } else if (fragmentLength - i >= 1) {
+                railLength += 1;
+            }
+            railLengths.put(i, railLength);
+        }
+        System.out.println(railLengths);
+
+        // Put all the letters in the cypher to their proper places as in the cleartext
+        int nextCharToProcess = 0;
+        for (int i = 0; i < fenceHeight; i++) {
+            for (int j = 0; j < railLengths.get(i); j++) {
+                char charToProcess = cypher.charAt(nextCharToProcess);
+                //int charPosition = offset + railDistance (depending on up/downstroke)
+            }
+        }
+
+        return null;
+    }
+
     /**
      * Calculates the distances in the plaintext for each level in the cipher on down and upstroke.
-     * @param fenceLevel the number of rails in the fence
+     * @param fenceHeight the number of rails in the fence
      * @return Map containing the fence distances for each level in the fence beginning with the top level
      */
-    private static Map calculateFenceDistances(int fenceLevel) {
+    private static Map<Integer, List<Integer>> calculateFenceDistances(int fenceHeight) {
         Map<Integer, List<Integer>> fenceDistances = new HashMap<Integer, List<Integer>>();
 
         // number of letters between two spike elements of the fence
-        int spikeDistance = 2 * fenceLevel - 2;
+        int spikeDistance = 2 * fenceHeight - 2;
 
         // determine distances in down/upstroke for each fence level
-        for (int i = 0; i < fenceLevel; i++) {
+        for (int i = 0; i < fenceHeight; i++) {
             List<Integer> fenceDistance = new ArrayList<Integer>();
             // special case for spikes
-            if (i == 0 || i == fenceLevel - 1) {
+            if (i == 0 || i == fenceHeight - 1) {
                 fenceDistance.add(spikeDistance);
                 fenceDistance.add(spikeDistance);
             } else {
